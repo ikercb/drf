0b9cfe1557f0d0d73827eb716a667ade37f4ecdc
ikercb <62053110+ikercb@users.noreply.github.com>
2021-05-18 10:06:39 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,5 +3,5 @@ from Cython.Build import cythonize
 import numpy
 
 setup(name="myCode", 
-      ext_modules=cythonize('myCode_cython21.pyx', language_level=3), 
+      ext_modules=cythonize('RF_cython.pyx', language_level=3), 
       include_dirs=[numpy.get_include()], zip_safe=False)
